e1ca840ddbbdff6ec15cf448ed73bc049f9d53c0
Garuka404 <garukagimhana@gmail.com>
2024-11-03 23:45:27 +0530

    Add user selection menu for number conversion options
    - Implemented menu with options for Decimal, Binary, Octal, Hexadecimal, and Roman Number converters.
    - Included validation for invalid option selection.
    - Enhanced console display with formatted headers for each converter type.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
         System.out.println(" __   ______             __");
         System.out.println("|  \\ /      \\           |  \\");
         System.out.println(" \\$$|  $$$$$$\\  ______  | $$  _______");
@@ -9,5 +13,43 @@ public class Main {
         System.out.println("| $$| $$__/  \\|  $$$$$$$| $$| $$_____");
         System.out.println("| $$|\\$$    $$ \\$$    $$| $$ \\$$     \\");
         System.out.println(" \\$$  \\$$$$$$   \\$$$$$$$ \\$$  \\$$$$$$$");
+
+        System.out.println("\n\n----------------------------------------------------");
+        System.out.println("\n\t[01] Decimal Converter \n\n\t[02] Binary Converter\n\n\t"
+                +"[03] Octal Converter \n\n\t[04] Hexadecimal Converter\n\n\t"
+                +"[05] Roman Number Converter");
+
+        System.out.print("\n\nEnter Option -> ");
+        int option=scan.nextInt();
+
+        if(option==1){
+            System.out.println("\n+---------------------------------------+");
+            System.out.println("|          Decimal Converter            |");
+            System.out.println("+---------------------------------------+");
+        }
+        else if(option==2){
+            System.out.println("\n+---------------------------------------+");
+            System.out.println("|          Binary Converter             |");
+            System.out.println("+---------------------------------------+");
+        }
+        else if(option==3){
+            System.out.println("\n+---------------------------------------+");
+            System.out.println("|           Octal Converter             |");
+            System.out.println("+---------------------------------------+");
+        }
+        else if(option==4){
+            System.out.println("\n+---------------------------------------+");
+            System.out.println("|         Hexadecimal Converter         |");
+            System.out.println("+---------------------------------------+");
+        }
+        else if(option==5){
+            System.out.println("\n+---------------------------------------+");
+            System.out.println("|         Roman Number Converter        |");
+            System.out.println("+---------------------------------------+");
+        }
+        else{
+            System.out.print("Invalid number try again!!!!!");
+        }
+
     }
 }
