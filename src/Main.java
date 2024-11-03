ce42956908e2525885a23bee7585a01967e77d23
Garuka404 <garukagimhana@gmail.com>
2024-11-03 23:09:40 +0530

    add ASCII art for icalc logo to console output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(" __   ______             __");
+        System.out.println("|  \\ /      \\           |  \\");
+        System.out.println(" \\$$|  $$$$$$\\  ______  | $$  _______");
+        System.out.println("|  \\| $$   \\$$ |      \\ | $$ /       \\");
+        System.out.println("| $$| $$        \\$$$$$$\\| $$|  $$$$$$$");
+        System.out.println("| $$| $$   __  /      $$| $$| $$");
+        System.out.println("| $$| $$__/  \\|  $$$$$$$| $$| $$_____");
+        System.out.println("| $$|\\$$    $$ \\$$    $$| $$ \\$$     \\");
+        System.out.println(" \\$$  \\$$$$$$   \\$$$$$$$ \\$$  \\$$$$$$$");
     }
 }
