d90f8281f97a904b1a99f74fab686385b2b2e2d7
Garuka404 <garukagimhana@gmail.com>
2024-11-04 23:07:41 +0530

    dd decimal to binary, octal, and hexadecimal conversion
    - Implemented conversion of a decimal number to binary, octal, and hexadecimal formats
    - Added validation for positive integer input and error handling for invalid inputs
    - Used user prompt to allow re-entry of numbers if an invalid input is detected
    - Added loop control with 'Y/N' prompt to continue or exit conversion process

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,85 @@ public class Main {
             System.out.println("\n+---------------------------------------+");
             System.out.println("|          Decimal Converter            |");
             System.out.println("+---------------------------------------+");
+            char again='Y';
+
+            while(true){
+                if(again=='Y'){
+                    String s="";
+                    System.out.print("\n\nEnter an Decimal number : ");
+                    int num=scan.nextInt();
+
+
+
+                    if(num>0){
+
+                        int  num1=0,num2=num;
+                        while(num2!=0){
+                            num1=num2%2;
+                            s=num1+s;
+                            num2=num2/2;
+                        }
+                        System.out.print("\n\tBinary number: "+s);
+                        s="";
+                        num1=0 ;
+                        num2=num;
+                        while(num2!=0){
+                            num1=num2%8;
+                            s=num1+s;
+
+                            num2=num2/8;
+                        }
+                        System.out.print("\n\tOctal number: "+s);
+                        s="";
+                        num1=0 ;
+                        num2=num;
+                        while(num2!=0){
+                            num1=num2%16;
+                            if(num1>=10){
+                                if(num1==10){
+                                    s="A"+s;
+                                }
+                                else if(num1==11){
+                                    s="B"+s;
+                                }
+                                else if(num1==12){
+                                    s="C"+s;
+                                }
+                                else if(num1==13){
+                                    s="D"+s;
+                                }
+                                else if(num1==14){
+                                    s="E"+s;
+                                }
+                                else{
+                                    s="F"+s;
+                                }
+                            }
+                            else{
+                                s=num1+s;
+                            }
+                            num2=num2/16;
+                        }
+                        System.out.print("\n\tHexadecimal number: "+s);
+                        break;
+
+
+
+                    }else{
+                        System.out.print("\n\tInvalid input..");
+
+                        System.out.print("\n\nDo you want to input number again(Y/N) ->  : ");
+                        again=scan.next().charAt(0);
+
+
+
+                    }
+                }else{
+                    System.out.print("no no no");
+                    break;
+                }
+            }
+
         }
         else if(option==2){
             System.out.println("\n+---------------------------------------+");
@@ -49,6 +128,8 @@ public class Main {
         }
         else{
             System.out.print("Invalid number try again!!!!!");
+
+
         }
 
     }
